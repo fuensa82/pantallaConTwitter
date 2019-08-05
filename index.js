e62149f7d8bf404a3c08989f60daa4241a168a2f
Victor Palomo <vPalomo@fuensalida.com>
2019-08-05 15:13:53 +0200

    Acceso a Twitter

--- index.js
+++ index.js
@@ -1,37 +1,28 @@
 var Twitter = require('twitter');
-//var Twitter = require('twitter-js-client').Twitter;
-//var Twitter = require('twit');
+
 var fs=require('fs');
 var variables={};
-/*fs.readFileSync('variables.json', 'utf8', function(err,data) {
-  if(err) throw err;
-  variables = JSON.parse(data);
-  console.log(variables);
-  console.log(data);
-});*/
+
 variables = JSON.parse(fs.readFileSync('variables.json', 'utf8'));
 
 var client = new Twitter({
-  consumer_key: variables.consumer_key,
-  consumer_secret: variables.consumer_secret,
-  access_token_key: variables.access_token_key,
-  access_token_secret: variables.access_token_secret,
-  access_token: variables.access_token_key,
-  timeout_ms:60*1000
+  consumer_key:"a1yK1VVXsyrXXG6xySdpwlG6z",
+  consumer_secret:"Lfu3nkaTbOYwewnNOecsouF0xrKtHTdacYlBX8wwcYWAZSosKT",
+  access_token_key: '155222158-9OKEzXzuoJnStWTK2mRwJUCL5FmSoXObjdIHchA5',
+  access_token_secret: 'Yl6LWiLJPPBwVW7FSFYtxpdb6nzhz29E87Ff0HOUAnvue'
 });
 
-/*client.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
-  console.log(data);
-});*/
-
-/*client.getUserTimeline({user_id: "155222158",count: '1'}, function(err){
-  console.log(err);
-},function(data){
-  console.log(data);
-});*/
-var params = {screen_name: 'nodejs'};
 client.get("statuses/home_timeline",function(error,tweets,response){
-  if(error) throw error;
+  if(error) 
+    throw error;
+  console.log(tweets);  // The favorites.
+  console.log(response);
+});
+var params = {q: '@AytoFuensalida'};
+//client.get("search/tweets",params,function(error,tweets,response){
+client.get("search/tweets",params,function(error,tweets,response){
+  if(error) 
+    throw error;
   console.log(tweets);  // The favorites.
   console.log(response);
 });
