e067c3fc6af3b78bf88b7696d6e56c631ca5122c
Victor Palomo <vPalomo@fuensalida.com>
2019-08-26 10:42:42 +0200

    Primeras versiones de pruebas

--- index.js
+++ index.js
@@ -11,16 +11,32 @@ var client = new Twitter({
   access_token_key: '155222158-9OKEzXzuoJnStWTK2mRwJUCL5FmSoXObjdIHchA5',
   access_token_secret: 'Yl6LWiLJPPBwVW7FSFYtxpdb6nzhz29E87Ff0HOUAnvue'
 });
-
-client.get("statuses/home_timeline",function(error,tweets,response){
+var params = {q: '@AytoFuensalida',count:5};
+//id de mensajes: 1158684792365473792
+//                1158025900664995841
+client.get("statuses/show/1158684792365473792",params,function(error,tweets,response){
   if(error) 
     throw error;
   console.log(tweets);  // The favorites.
   console.log(response);
 });
-var params = {q: '@AytoFuensalida'};
-//client.get("search/tweets",params,function(error,tweets,response){
+/*client.get("statuses/home_timeline",function(error,tweets,response){
+  if(error) 
+    throw error;
+  console.log(tweets);  // The favorites.
+  console.log(response);
+});*/
+var params = {q: 'fuensa82fonera',count:5};
 client.get("search/tweets",params,function(error,tweets,response){
+  if(error) 
+    throw error;
+  console.log(tweets);  // The favorites.
+  console.log(response);
+});
+//direct_messages/events/list
+params={user_id:"318965512"};//{screen_name: username, count:100  }
+params={screen_name: "AytoFuensalida", count:2}
+client.get("statuses/user_timeline",params,function(error,tweets,response){
   if(error) 
     throw error;
   console.log(tweets);  // The favorites.
